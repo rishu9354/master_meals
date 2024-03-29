1d7a0843f2b320cf64cc43e6b12c60b95ea815d9
Rishabh Sharma <113090549+rishu9354@users.noreply.github.com>
2024-03-29 16:35:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
     document.querySelector('.loader-container').style.display = 'none';
     document.querySelector('.content').classList.remove('hidden');
-  }, 10000);
+  }, 20000);
 });
